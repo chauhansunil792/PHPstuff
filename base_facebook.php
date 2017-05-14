fdfc8aa98e5e20e2c104d73280fd8dfa3394b9a8
Sunil Chauhan <chauhansunil792@gmail.com>
2017-05-14 06:12:58 -0700

    Update base_facebook.php

--- base_facebook.php
+++ base_facebook.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * Copyright 2011 Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 
 if (!function_exists('curl_init')) {
   throw new Exception('Facebook needs the CURL PHP extension.');
@@ -22,11 +7,7 @@ if (!function_exists('json_decode')) {
   throw new Exception('Facebook needs the JSON PHP extension.');
 }
 
-/**
- * Thrown when an API call returns an exception.
- *
- * @author Naitik Shah <naitik@facebook.com>
- */
+
 class FacebookApiException extends Exception
 {
   /**
@@ -36,7 +17,7 @@ class FacebookApiException extends Exception
 
   /**
    * Make a new API Exception with the given result.
-   *
+   * author: sunil chauhan
    * @param array $result The result from the API server
    */
   public function __construct($result) {
@@ -72,7 +53,7 @@ class FacebookApiException extends Exception
   /**
    * Returns the associated type for the error. This will default to
    * 'Exception' when a type is not available.
-   *
+   * author: sunil chauhan
    * @return string
    */
   public function getType() {
@@ -113,7 +94,7 @@ class FacebookApiException extends Exception
  * implement the three abstract methods listed at the bottom of
  * the file.
  *
- * @author Naitik Shah <naitik@facebook.com>
+ *
  */
 abstract class BaseFacebook
 {
@@ -189,6 +170,7 @@ abstract class BaseFacebook
    * code.  null means the access token has yet to be determined.
    *
    * @var string
+   * author: sunil chauhan
    */
   protected $accessToken = null;
 
